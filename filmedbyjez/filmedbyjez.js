c567acab21a31d98bc684a605cdd98823f6d79c9
Tom Yates <yatesy07@live.co.uk>
2020-09-16 17:45:20 +0100

    Added (See Desc)
    - Removed all placeholder text for real text.
    
    - Added Back To Top button
    
    - Added Smooth Scroll

--- filmedbyjez/filmedbyjez.js
+++ filmedbyjez/filmedbyjez.js
@@ -1,9 +1,31 @@
+//Photos Carousel
 $('#text').html($('.active > .pics').html());
 $('.carousel').on('slid.bs.carousel', function () {
   	$('#text').html($('.active > .pics').html());
     });
-    
+
+  //Vids Carousel
 $('#text2').html($('.active > .vids').html());
 $('.carousel').on('slid.bs.carousel', function () {
         $('#text2').html($('.active > .vids').html());
     });
+
+
+//Back To Top Button    
+//Get the button
+var mybutton = document.getElementById("myBtn");
+//Show button after 20px Scroll
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
