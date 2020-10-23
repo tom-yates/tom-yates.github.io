2dab8ef57ff628234d14ebb133105aa98e06ed11
Tom Yates <yatesy07@live.co.uk>
2020-10-23 16:39:36 +0100

    php test

---
+++ testing.php
@@ -0,0 +1,37 @@
+<?php
+$email_address = $_POST['email_address'];
+$feedback = $_POST['feedback'];
+
+function filter_email_header($form_field) {
+    return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
+}
+
+$email_address = filter_email_header($email_address);
+
+$headers = "From: $email_addressn";
+$sent = mail('yatesy07@live.co.uk', 'Feedback Form Submission', $feedback, $headers);
+
+if ($sent) {
+    ?><html>
+        <head>
+            <title>Thank You</title>
+        </head>
+        <body>
+            <h1>Thank You</h1>
+            <p>thanks for your feedback</p>
+        </body>
+    </html>
+    <?php
+} else {
+    ?><html>
+        <head>
+            <title>Something went wrong</title>
+        </head>
+        <body>
+            <h1>Something went wrong</h1>
+            <p>we could not send</p>
+        </body>
+    </html>
+    <?php
+}
+?>
